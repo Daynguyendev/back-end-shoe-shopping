aa1a196f47824e79d82e7b014982e4a2f955927d
Daynguyendev <daynguyendev@gmail.com>
2023-04-10 01:38:08 +0700

    updateNewversion

--- app/models/db.js
+++ app/models/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -7,6 +7,7 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME,
     port: process.env.DB_PORT
 });
+
 connection.connect(error => {
     if (error) throw error;
     console.log("Ket noi database thanh cong");
