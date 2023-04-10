9e724a8540b361f54a0c88ba99eebc0f25fc7500
Daynguyendev <daynguyendev@gmail.com>
2023-04-10 10:39:22 +0700

    deloyCheckout

--- app/config/data.js
+++ app/config/data.js
@@ -4,5 +4,5 @@ module.exports = {
   vnp_HashSecret: "ESHXKPBNXQXUMNALCWAFBEOIEDQPAMHB",
   vnp_Url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
   vnp_Api: "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction",
-  vnp_ReturnUrl: "http://localhost:5000/order/vnpay_ipn",
+  vnp_ReturnUrl: "https://daynguyen-backend-shoe-shopping.onrender.com/order/vnpay_ipn",
 }
